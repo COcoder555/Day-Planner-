600ac6178965ebce971f990abfbaccbe720d0208
COcoder555 <cocoder555@gmail.com>
2021-05-19 17:01:37 -0600

    Created and commented a fuction for save buttons to save the text to local storage

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 // Grabbed the id attrtribute in the Header and store it in variable
 // Then changed the text content using Moment to the current day
 console.log("Am I working")
-const $CurrentDay = $("#currentDay"); 
+const $CurrentDay = $("#currentDay");
 $CurrentDay.text(moment().format("MMMM Do YYYY"));
 
 // Grabbed all the elements with a class of ".saveBtn and stored them 
@@ -10,34 +10,37 @@ $CurrentDay.text(moment().format("MMMM Do YYYY"));
 // Created a for loop that loops through all the newely formed array and added an EventListnet to them that 
 // would react with a "click" and that would call "Save function"
 $buttons = document.querySelectorAll(".saveBtn");
-for(i =0; i < $buttons.length; i++){
-    $buttons[i].addEventListener("click",Save)
-    };
+for (i = 0; i < $buttons.length; i++) {
+    $buttons[i].addEventListener("click", Save)
+};
 
-    //  Created a save function.
-    // In the funciton I created two variables 
-    //  Usint "this" I stored the value of the "text area" in a varible called "Input"
-    // 
-function Save(){
- let Input =  $(this).siblings("textarea").val();
- let Key = $(this).siblings("textarea").attr("id");
- localStorage.setItem(Key,Input);
+//  Created a save function.
+// In the funciton I created two variables 
+//  Using "this" I stored the value of the "text area" in a varible called "Input"
+//  Usidng "this" I stored the id's of the "text area" in a variable called "Key"
+//  I stored both Key an Imput in localStorage
+function Save() {
+    let Input = $(this).siblings("textarea").val();
+    let Key = $(this).siblings("textarea").attr("id");
+    localStorage.setItem(Key, Input);
 
 }
 
+// Grabbed all the text area elements amd stored them in a variable "$textarea"
 let $textarea = document.querySelectorAll("textarea");
 
-function Saved(){
-for(i=0; i <$textarea.length; i ++)
-$textarea[i].textContent= localStorage.getItem($textarea[i].id);
+
+function Saved() {
+    for (i = 0; i < $textarea.length; i++)
+        $textarea[i].textContent = localStorage.getItem($textarea[i].id);
 };
 
 Saved();
 
-for(i=0; i<$textarea.length; i++){
-    let currentName =parseInt( $textarea[i].name);
+for (i = 0; i < $textarea.length; i++) {
+    let currentName = parseInt($textarea[i].name);
     let currentTime = parseInt(moment().format("HH"));
-    if(currentName === currentTime){$textarea[i].className = "present col-10"}
-    else if(currentName < currentTime){$textarea[i].className= "past col-10"}
-    else if(currentName > currentTime){$textarea[i].className= "future col-10"}
+    if (currentName === currentTime) { $textarea[i].className = "present col-10" }
+    else if (currentName < currentTime) { $textarea[i].className = "past col-10" }
+    else if (currentName > currentTime) { $textarea[i].className = "future col-10" }
 };
